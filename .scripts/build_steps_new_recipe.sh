f837ed071905f9f9ddb08bdb82700859d570aeef
nichmor <nmorkotilo@gmail.com>
2024-03-20 12:52:22 +0200

    misc: redirect them to files

--- .scripts/build_steps_new_recipe.sh
+++ .scripts/build_steps_new_recipe.sh
@@ -43,8 +43,8 @@ fi
 
 # rattler-build currently don't support conda debug, so I removed debug section
 
-rattler-build build --recipe "${RECIPE_ROOT}" --render-only --target-platform linux-64 -m "${RECIPE_ROOT}/variants.yaml"
-rattler-build build --recipe "${RECIPE_ROOT}" --render-only --target-platform osx-64 -m "${RECIPE_ROOT}/variants.yaml"
+rattler-build build --recipe "${RECIPE_ROOT}" --render-only --target-platform linux-64 -m "${RECIPE_ROOT}/variants.yaml" > linux.json
+rattler-build build --recipe "${RECIPE_ROOT}" --render-only --target-platform osx-64 -m "${RECIPE_ROOT}/variants.yaml" > osx.json
 
 ( startgroup "Final checks" ) 2> /dev/null
 
